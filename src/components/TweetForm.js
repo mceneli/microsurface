89f24634f80a643dc53a610dbb253508555321b5
Muhammet Çeneli <muhammetceneli@gmail.com>
2023-11-14 17:17:47 +0300

    fotoğraf eklendi

--- src/components/TweetForm.js
+++ src/components/TweetForm.js
@@ -4,6 +4,9 @@ import { decodeToken } from '../util/Util';
 const TweetForm = () => {
     const [tweetContent, setTweetContent] = useState('');
     const [userName, setUserName] = useState('');
+    const [image, setImage] = useState(null);
+    const [selectedImage, setSelectedImage] = useState(null);
+
     let token = null;
     if(localStorage.getItem('token')){
       token = decodeToken(localStorage.getItem('token'));
@@ -11,24 +14,25 @@ const TweetForm = () => {
     
     useEffect(() => {
       if (token) {
-        
         setUserName(token["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"]);
       }    
     }, [token]);
     
       const sendTweet = async () => {
         try {
-          const data = { username: userName, text: tweetContent};
+          const data = new FormData();
+          data.append('username', userName);
+          data.append('text', tweetContent);
+          data.append('image', image);
           
           const response = await fetch(process.env.REACT_APP_API_ENDPOINT+"/api/tweets/CreateTweet", {
             method: 'POST',
             headers: {
-              'Content-Type': 'application/json',
               Authorization: 'Bearer ' + localStorage.getItem('token')
             },
-            body: JSON.stringify(data)
+            body: data
           });
-          if(response){
+          if(response.ok){
             console.log("Tweet Created.");
           }
         } catch (error) {
@@ -36,7 +40,23 @@ const TweetForm = () => {
         }
       
       };
+
+      const handleImageChange = (event) => {
+        const file = event.target.files[0];
     
+        setImage(file)
+        if (file) {
+          // Resmi önizleme yapmak için Base64'e dönüştür
+          const reader = new FileReader();
+          reader.onloadend = () => {
+            setSelectedImage(reader.result);
+          };
+          reader.readAsDataURL(file);
+        } else {
+          setSelectedImage(null);
+        }
+      };
+
   return (
     <div className="tweet-form-container">
     <label htmlFor="userName">Kullanıcı Adı: {userName}</label>
@@ -50,6 +70,17 @@ const TweetForm = () => {
       rows="4"
       required
     ></textarea>
+
+    {selectedImage || 1 ? (
+      <div><div>
+        <img src={selectedImage} alt="Seçilen Resim" style={{ maxWidth: '100%' }} />
+      </div>
+      <label htmlFor="image">Resim Ekle: 
+        <input type="file" id="image" accept="image/*" onChange={handleImageChange} />
+      </label></div>
+    ):(
+      <label>asd</label>
+    )}
 
     <button type="button" className="send-button" onClick={sendTweet}>
       Tweet Gönder
